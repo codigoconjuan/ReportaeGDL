501adcd24f16b537f0eca8e5ede74dffa9effa74
juanpablogdl <juanpablodelatorrevaldez@iMac-de-Juan.local>
2014-10-27 23:40:08 -0600

    nuevos ajustes
    nuevos ajustes

---
+++ src/com/reporta/reportaegdl/BacheActivity.java
@@ -0,0 +1,14 @@
+package com.reporta.reportaegdl;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BacheActivity extends Activity {
+
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.bache_activity);
+	}
+	
+}
