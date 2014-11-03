b30b1dc2281da95b14f17a84b95f1af0cda3a3c1
juanpablogdl <juanpablodelatorrevaldez@iMac-de-Juan.local>
2014-11-02 20:36:40 -0600

    nuevos ajustes
    nuevos ajuste

--- gen/com/reporta/reportaegdl/R.java
+++ gen/com/reporta/reportaegdl/R.java
@@ -27,26 +27,35 @@ public final class R {
         public static final int mtop=0x7f040002;
     }
     public static final class drawable {
-        public static final int btn_arboles=0x7f020000;
-        public static final int btn_baches=0x7f020001;
-        public static final int btn_fugas=0x7f020002;
-        public static final int btn_luminarias=0x7f020003;
-        public static final int ic_launcher=0x7f020004;
-        public static final int logo=0x7f020005;
+        public static final int bg_azul=0x7f020000;
+        public static final int bg_normal=0x7f020001;
+        public static final int bg_presionado=0x7f020002;
+        public static final int bg_rosa=0x7f020003;
+        public static final int botones=0x7f020004;
+        public static final int botonrosa=0x7f020005;
+        public static final int btn_arboles=0x7f020006;
+        public static final int btn_baches=0x7f020007;
+        public static final int btn_fugas=0x7f020008;
+        public static final int btn_luminarias=0x7f020009;
+        public static final int btntxt=0x7f02000a;
+        public static final int ic_action_camara=0x7f02000b;
+        public static final int ic_launcher=0x7f02000c;
+        public static final int logo=0x7f02000d;
     }
     public static final class id {
-        public static final int ImageButton01=0x7f090003;
-        public static final int ImageButton02=0x7f090004;
-        public static final int ImageButton03=0x7f090002;
-        public static final int ImageButton04=0x7f090001;
-        public static final int action_settings=0x7f09000b;
+        public static final int ImageButton01=0x7f090004;
+        public static final int ImageButton02=0x7f090005;
+        public static final int ImageButton03=0x7f090003;
+        public static final int ImageButton04=0x7f090002;
+        public static final int action_settings=0x7f09000c;
+        public static final int btn_guardar=0x7f09000a;
         public static final int button1=0x7f090006;
-        public static final int button2=0x7f090007;
-        public static final int button3=0x7f09000a;
-        public static final int editText1=0x7f090009;
+        public static final int button3=0x7f090009;
+        public static final int editText1=0x7f090008;
+        public static final int fotoBoton=0x7f090007;
         public static final int imageView1=0x7f090000;
-        public static final int textView1=0x7f090005;
-        public static final int textView2=0x7f090008;
+        public static final int textView1=0x7f090001;
+        public static final int textView2=0x7f09000b;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -54,7 +63,6 @@ public final class R {
         public static final int bache_activity=0x7f030002;
         public static final int fuga_activity=0x7f030003;
         public static final int luminaria_activity=0x7f030004;
-        public static final int nuevo_layout=0x7f030005;
     }
     public static final class menu {
         public static final int main=0x7f080000;
@@ -68,8 +76,10 @@ public final class R {
         public static final int enviar=0x7f050007;
         public static final int foto=0x7f050006;
         public static final int fuga=0x7f050009;
+        public static final int guardar=0x7f05000b;
         public static final int hello_world=0x7f050001;
         public static final int luminaria=0x7f050008;
+        public static final int reportar=0x7f05000c;
         public static final int ubicacion=0x7f050004;
     }
     public static final class style {
--- src/com/reporta/reportaegdl/FugaActivity.java
+++ src/com/reporta/reportaegdl/FugaActivity.java
@@ -1,16 +1,45 @@
 package com.reporta.reportaegdl;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class FugaActivity extends Activity {
-
+	
+	ImageView iv;
+	
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.fuga_activity);
 		getActionBar().setDisplayHomeAsUpEnabled(true);
+		
+		iv = (ImageView) findViewById(R.id.imageView1);
+		
+		Button b = (Button) findViewById(R.id.fotoBoton);
+		b.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+				startActivityForResult(intent, 0);
+			}
+		});
+	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		// TODO Auto-generated method stub
+		super.onActivityResult(requestCode, resultCode, data);
+		
+		Bitmap bm = (Bitmap) data.getExtras().get("data");
+		
+		iv.setImageBitmap(bm);
 	}
 	
 	
